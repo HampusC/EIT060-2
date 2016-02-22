959a890f9984cb4b1131014697afca44e9888caa
Simon Nilsson <dat13sni@jupiter-12.student.lth.se>
2016-02-22 16:29:31 +0100

    added more details to recordviewer in client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,11 +1,15 @@
 package client;
 
 import java.net.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.io.*;
 import javax.net.ssl.*;
 import javax.security.cert.X509Certificate;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import types.Record;
 
@@ -23,114 +27,132 @@ import java.util.Scanner;
  */
 public class Client {
 
-    public static void main(String[] args) throws Exception {
-        String host = null;
-        int port = -1;
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("args[" + i + "] = " + args[i]);
-        }
-        if (args.length < 2) {
-            System.out.println("USAGE: java client host port");
-            System.exit(-1);
-        }
-        try { /* get input parameters */
-            host = args[0];
-            port = Integer.parseInt(args[1]);
-           Client client = new Client(host, port);
-        } catch (IllegalArgumentException e) {
-            System.out.println("USAGE: java client host port");
-            System.exit(-1);
-        }
+	public static void main(String[] args) throws Exception {
+		String host = null;
+		int port = -1;
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("args[" + i + "] = " + args[i]);
+		}
+		if (args.length < 2) {
+			System.out.println("USAGE: java client host port");
+			System.exit(-1);
+		}
+		try { /* get input parameters */
+			host = args[0];
+			port = Integer.parseInt(args[1]);
+			Client client = new Client(host, port);
+		} catch (IllegalArgumentException e) {
+			System.out.println("USAGE: java client host port");
+			System.exit(-1);
+		}
 
- 
-    }
-    public Client(String host, int port) throws Exception{
-        try { /* set up a key manager for client authentication */
-            SSLSocketFactory factory = null;
-            try {
-                Scanner scan = new Scanner(System.in);
-                System.out.println("Input the filepath to your certificate");
-                String filePath = scan.nextLine();
-                char[] password = "password".toCharArray();
-                System.out.println("Input your password");
-                String userPass = scan.nextLine();
-                KeyStore ks = KeyStore.getInstance("JKS");
-                KeyStore ts = KeyStore.getInstance("JKS");
-                KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-                TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-                SSLContext ctx = SSLContext.getInstance("TLS");
-                ks.load(new FileInputStream("src/client/clientkeystore"), password);  // keystore password (storepass)
-				ts.load(new FileInputStream("src/client/clienttruststore"), password); // truststore password (storepass);
+	}
+
+	public Client(String host, int port) throws Exception {
+		try { /* set up a key manager for client authentication */
+			SSLSocketFactory factory = null;
+			try {
+				Scanner scan = new Scanner(System.in);
+				System.out.println("Input the filepath to your certificate");
+				String filePath = scan.nextLine();
+				char[] password = "password".toCharArray();
+				System.out.println("Input your password");
+				String userPass = scan.nextLine();
+				KeyStore ks = KeyStore.getInstance("JKS");
+				KeyStore ts = KeyStore.getInstance("JKS");
+				KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+				TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+				SSLContext ctx = SSLContext.getInstance("TLS");
+				ks.load(new FileInputStream("src/client/clientkeystore"), password); // keystore
+																						// password
+																						// (storepass)
+				ts.load(new FileInputStream("src/client/clienttruststore"), password); // truststore
+																						// password
+																						// (storepass);
 				kmf.init(ks, userPass.toCharArray()); // user password (keypass)
 				tmf.init(ts); // keystore can be used as truststore here
 				ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
-                factory = ctx.getSocketFactory();
-            } catch (Exception e) {
-                throw new IOException(e.getMessage());
-            }
-            SSLSocket socket = (SSLSocket)factory.createSocket(host, port);
-            System.out.println("\nsocket before handshake:\n" + socket + "\n");
+				factory = ctx.getSocketFactory();
+			} catch (Exception e) {
+				throw new IOException(e.getMessage());
+			}
+			SSLSocket socket = (SSLSocket) factory.createSocket(host, port);
+			System.out.println("\nsocket before handshake:\n" + socket + "\n");
 
-            /*
-             * send http request
-             *
-             * See SSLSocketClient.java for more information about why
-             * there is a forced handshake here when using PrintWriters.
-             */
-            socket.startHandshake();
+			/*
+			 * send http request
+			 *
+			 * See SSLSocketClient.java for more information about why there is
+			 * a forced handshake here when using PrintWriters.
+			 */
+			socket.startHandshake();
 
-            SSLSession session = socket.getSession();
-            X509Certificate cert = (X509Certificate)session.getPeerCertificateChain()[0];
-            String subject = cert.getSubjectDN().getName();
-            String issuer = cert.getIssuerDN().getName();
-            String serial = cert.getSerialNumber().toString();
-            System.out.println("certificate name (subject DN field) on certificate received from server:\n" + subject + "\n");
-            System.out.println("certificate issuer (issuer DN field) on certificate received from server:\n" + issuer + "\n");
-            System.out.println("certificate serial number (serial number field) on certificate received from server:\n" + serial + "\n");
-            System.out.println("socket after handshake:\n" + socket + "\n");
-            System.out.println("secure connection established\n\n");
+			SSLSession session = socket.getSession();
+			X509Certificate cert = (X509Certificate) session.getPeerCertificateChain()[0];
+			String subject = cert.getSubjectDN().getName();
+			String issuer = cert.getIssuerDN().getName();
+			String serial = cert.getSerialNumber().toString();
+			System.out.println(
+					"certificate name (subject DN field) on certificate received from server:\n" + subject + "\n");
+			System.out.println(
+					"certificate issuer (issuer DN field) on certificate received from server:\n" + issuer + "\n");
+			System.out.println("certificate serial number (serial number field) on certificate received from server:\n"
+					+ serial + "\n");
+			System.out.println("socket after handshake:\n" + socket + "\n");
+			System.out.println("secure connection established\n\n");
 
-            BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String msg;
+			BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String msg;
 			for (;;) {
-                System.out.print(">");
-                msg = read.readLine();
-                if (msg.equalsIgnoreCase("quit")) {
-				    break;
+				System.out.print(">");
+				msg = read.readLine();
+				if (msg.equalsIgnoreCase("quit")) {
+					break;
 				}
-                System.out.print("sending '" + msg + "' to server...");
-                out.println(msg);
-                out.flush();
-                System.out.println("done");
-                String response=in.readLine();
-                if(response.equals("sending journal")){
-                	reciveJournal(in.readLine());
-                }else{
-                	
-                System.out.println("received '" + response+ "' from server\n");
-                }
-            }
-            in.close();
+				System.out.print("sending '" + msg + "' to server...");
+				out.println(msg);
+				out.flush();
+				System.out.println("done");
+				String response = in.readLine();
+				if (response.equals("sending journal")) {
+					reciveJournal(in.readLine());
+				} else {
+
+					System.out.println("received '" + response + "' from server\n");
+				}
+			}
+			in.close();
 			out.close();
 			read.close();
-            socket.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	private void reciveJournal(String string) {
 		JFrame f = new JFrame("Journal Viewer");
-		f.setSize(600, 600);
+		f.setSize(600, 400);
 		JLabel dataLabel = new JLabel();
 		dataLabel.setText(string);
-		f.add(dataLabel);
+		JLabel infoLabel = new JLabel("Doctor: Matts " + " Nurse: Lisa");
+		JLabel divLabel = new JLabel("Dvision: Div 3");
+		JLabel dateLabel = new JLabel("2014-0-12");
+		JPanel infoPanel = new JPanel();
+		dataLabel.setBackground(Color.WHITE);
+		infoPanel.setLayout(new BorderLayout());
+		infoPanel.add(dateLabel, BorderLayout.NORTH);
+		infoPanel.add(divLabel, BorderLayout.CENTER);
+		infoPanel.add(infoLabel, BorderLayout.SOUTH);
+		infoPanel.setBackground(Color.RED);
+		dataLabel.setBorder(BorderFactory.createLoweredBevelBorder());
+		;
+		f.add(infoPanel, BorderLayout.NORTH);
+		f.add(dataLabel, BorderLayout.CENTER);
 		f.setVisible(true);
-		
-		
-		
-		
+
 	}
 
 }
