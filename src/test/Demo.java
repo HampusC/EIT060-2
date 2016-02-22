74ffe94d78673687fdd4849ad5875f2e42dfbb3e
Simon Nilsson <dat13sni@mars-11.student.lth.se>
2016-02-22 14:08:09 +0100

    added demo

---
+++ src/test/Demo.java
@@ -0,0 +1,42 @@
+package test;
+import server.Server;
+import client.Client;
+public class Demo {
+
+			public static void main(String[] args) {
+				Server1 s = new Server1();
+				s.start();
+				try {
+					Thread.currentThread().sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				Client1 c = new Client1();
+				c.start();
+			
+			
+			}
+			
+			private static class Server1 extends Thread {
+				public void run() {
+					Server.main(new String[] {"9876"});
+				}
+			}
+			
+		
+			
+			private static class Client1 extends Thread {
+				public void run() {
+					try {
+						Client.main(new String[] {"localhost" , "9876"});
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+
+
+	}
+
+
